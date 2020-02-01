29089c5e28075851fb1f1a0c5191ad14b3bf17be
unknown <david.jaroska@gmail.com>
2020-02-01 14:40:18 +0100

    finished codereview3

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function calculateInsurance() {
+	var x = parseInt(document.getElementById("myAge").value);
+	var y = document.getElementById("horsepower").value;
+	var z = document.getElementById("country").value;
+	var name = document.getElementById("myName").value;
+	if(z=='Austria'){
+	document.getElementById("demo").innerHTML =name +", your insurance costs "  + (y*100/ x + 50) + "€" ;
+}	else if (z=='Hungaria') {
+	document.getElementById("demo").innerHTML =name +", your insurance costs "  + Math.round((y*120/ x + 100)) + "€"
+}
+	else{
+		document.getElementById("demo").innerHTML =name +", your insurance costs "  + Math.round((y*150/ (x+3) + 50)) + "€"
+}
+}
